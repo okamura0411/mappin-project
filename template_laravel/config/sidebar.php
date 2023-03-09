49a148ea08468e738e7a04cea7acd14188e0e612
okamura0411 <s16e01501195@gmail.come>
2023-03-09 16:22:16 +0900

    route整理

--- template_laravel/config/sidebar.php
+++ template_laravel/config/sidebar.php
@@ -2,35 +2,8 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | View Storage Paths
-    |--------------------------------------------------------------------------
-    |
-    | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be checked for your views. Of course
-    | the usual Laravel view path has already been registered for you.
-    |
-    */
-  'menu' => [[
-		'icon' => 'fa fa-sitemap',
-		'title' => 'Dashboard',
-		'url' => 'javascript:;',
-		'caret' => true,
-		'sub_menu' => [[
-			'url' => '/dashboard/v1',
-			'title' => 'Dashboard v1',
-			'route-name' => 'dashboard-v1'
-		],[
-			'url' => '/dashboard/v2',
-			'title' => 'Dashboard v2',
-			'route-name' => 'dashboard-v2'
-		],[
-			'url' => '/dashboard/v3',
-			'title' => 'Dashboard v3',
-			'route-name' => 'dashboard-v3'
-		]]
-		],[
+  'menu' => [
+		[
 		'icon' => 'fa fa-sitemap',
 		'title' => 'dashboard/v1',
 		'url' => '/dashboard/v1',
@@ -47,6 +20,11 @@ return [
 		'route-name' => 'dashboard-v3'
 	],[
 		'icon' => 'fa fa-sitemap',
+		'title' => 'subway',
+		'url' => '/system',
+		'route-name' => 'subway'
+	],[
+		'icon' => 'fa fa-sitemap',
 		'title' => 'Calendar',
 		'url' => '/calendar',
 		'route-name' => 'calendar'
@@ -59,52 +37,6 @@ return [
 			'url' => '/gallery/v1',
 			'title' => 'Gallery v1',
 			'route-name' => 'gallery-v1'
-		]]
-	],[
-		'icon' => 'fa fa-key',
-		'title' => 'Login & Register',
-		'url' => 'javascript:;',
-		'caret' => true,
-		'sub_menu' => [[
-			'url' => '/login/v3',
-			'title' => 'Login v3',
-			'route-name' => 'login-v3'
-		],[
-			'url' => '/register/v3',
-			'title' => 'Register v3',
-			'route-name' => 'register-v3'
-		]]
-	],[
-		'icon' => 'fa fa-align-left',
-		'title' => 'Menu Level',
-		'url' => 'javascript:;',
-		'caret' => true,
-		'sub_menu' => [[
-			'url' => 'javascript:;',
-			'title' => 'Menu 1.1',
-			'sub_menu' => [[
-				'url' => 'javascript:;',
-				'title' => 'Menu 2.1',
-				'sub_menu' => [[
-					'url' => 'javascript:;',
-					'title' => 'Menu 3.1',
-				],[
-					'url' => 'javascript:;',
-					'title' => 'Menu 3.2'
-				]]
-			],[
-				'url' => 'javascript:;',
-				'title' => 'Menu 2.2'
-			],[
-				'url' => 'javascript:;',
-				'title' => 'Menu 2.3'
-			]]
-		],[
-			'url' => 'javascript:;',
-			'title' => 'Menu 1.2'
-		],[
-			'url' => 'javascript:;',
-			'title' => 'Menu 1.3'
 		]]
 	]]
 ];
