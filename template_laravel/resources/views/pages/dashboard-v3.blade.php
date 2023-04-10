d610a0e4366e45a4c1693f000af5204e2c18cac2
okamura0411 <s16e01501195@gmail.come>
2023-04-10 22:46:07 +0900

    asset

--- template_laravel/resources/views/pages/dashboard-v3.blade.php
+++ template_laravel/resources/views/pages/dashboard-v3.blade.php
@@ -209,6 +209,6 @@
 		<div id="preview">
 		ここに画像が表示される。
 		</div>
-		<script src="/assets/js/demo/dashboard-v3.js"></script>
-
+		{{-- <script src="/assets/js/demo/dashboard-v3.js"></script> --}}
+		<script src="{{ asset('assets/js/demo/dashboard-v3.js') }}"></script>
 @endsection
